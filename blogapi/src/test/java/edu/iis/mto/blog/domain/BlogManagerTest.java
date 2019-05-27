b4986b3757b702cc51f84e34be8cc8ee93954223
adamsylla97 <209474@edu.p.lodz.pl>
2019-05-27 21:02:27 +0200

    3.2 added test, probably error in code

--- blogapi/src/test/java/edu/iis/mto/blog/domain/BlogManagerTest.java
+++ blogapi/src/test/java/edu/iis/mto/blog/domain/BlogManagerTest.java
@@ -1,10 +1,16 @@
 package edu.iis.mto.blog.domain;
 
+import edu.iis.mto.blog.domain.errors.DomainError;
+import edu.iis.mto.blog.domain.model.BlogPost;
+import edu.iis.mto.blog.domain.model.LikePost;
+import edu.iis.mto.blog.domain.repository.BlogPostRepository;
+import edu.iis.mto.blog.domain.repository.LikePostRepository;
 import org.hamcrest.Matchers;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -18,12 +24,22 @@ import edu.iis.mto.blog.domain.repository.UserRepository;
 import edu.iis.mto.blog.mapper.BlogDataMapper;
 import edu.iis.mto.blog.services.BlogService;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class BlogManagerTest {
 
     @MockBean
     UserRepository userRepository;
+
+    @MockBean
+    private LikePostRepository postRepository;
+
+    @MockBean
+    private BlogPostRepository blogRepository;
 
     @Autowired
     BlogDataMapper dataMapper;
@@ -38,6 +54,84 @@ public class BlogManagerTest {
         Mockito.verify(userRepository).save(userParam.capture());
         User user = userParam.getValue();
         Assert.assertThat(user.getAccountStatus(), Matchers.equalTo(AccountStatus.NEW));
+    }
+
+    @Test
+    public void userWithConfirmedAccoundShouldBeAbleToLikePost(){
+
+        User postUser = new User();
+        postUser.setFirstName("Jan");
+        postUser.setLastName("Kowalski");
+        postUser.setEmail("jan.kowalski@interia.pl");
+        postUser.setId(0L);
+        postUser.setAccountStatus(AccountStatus.NEW);
+        userRepository.save(postUser);
+        Mockito.when(userRepository.findById(0L)).thenReturn(Optional.of(postUser));
+
+        User likeUser = new User();
+        likeUser.setFirstName("Dawid");
+        likeUser.setLastName("Nowak");
+        likeUser.setEmail("dawid.nowak@yahoo.com");
+        likeUser.setId(1L);
+        likeUser.setAccountStatus(AccountStatus.CONFIRMED);
+        userRepository.save(likeUser);
+        Mockito.when(userRepository.findById(1L)).thenReturn(Optional.of(likeUser));
+
+        BlogPost blogPost = new BlogPost();
+        blogPost.setUser(postUser);
+        blogPost.setEntry("abcde");
+        blogPost.setId(0L);
+        Mockito.when(blogRepository.findById(0L)).thenReturn(Optional.of(blogPost));
+
+        Mockito.when(postRepository.findByUserAndPost(likeUser,blogPost)).thenReturn(Optional.empty());
+        blogService.addLikeToPost(likeUser.getId(),blogPost.getId());
+
+        ArgumentCaptor<LikePost> captor = ArgumentCaptor.forClass(LikePost.class);
+        Mockito.verify(postRepository).save(captor.capture());
+
+        LikePost likePost = captor.getValue();
+        Assert.assertEquals(likePost.getPost(),blogPost);
+        Assert.assertEquals(likePost.getUser(),likeUser);
+
+    }
+
+    @Test (expected = DomainError.class)
+    public void userWithoutConfirmedAccoundShouldNotBeAbleToLikePost(){
+
+        User postUser = new User();
+        postUser.setFirstName("Jan");
+        postUser.setLastName("Kowalski");
+        postUser.setEmail("jan.kowalski@interia.pl");
+        postUser.setId(0L);
+        postUser.setAccountStatus(AccountStatus.NEW);
+        userRepository.save(postUser);
+        Mockito.when(userRepository.findById(0L)).thenReturn(Optional.of(postUser));
+
+        User likeUser = new User();
+        likeUser.setFirstName("Dawid");
+        likeUser.setLastName("Nowak");
+        likeUser.setEmail("dawid.nowak@yahoo.com");
+        likeUser.setId(1L);
+        likeUser.setAccountStatus(AccountStatus.NEW);
+        userRepository.save(likeUser);
+        Mockito.when(userRepository.findById(1L)).thenReturn(Optional.of(likeUser));
+
+        BlogPost blogPost = new BlogPost();
+        blogPost.setUser(postUser);
+        blogPost.setEntry("abcde");
+        blogPost.setId(0L);
+        Mockito.when(blogRepository.findById(0L)).thenReturn(Optional.of(blogPost));
+
+        Mockito.when(postRepository.findByUserAndPost(likeUser,blogPost)).thenReturn(Optional.empty());
+        blogService.addLikeToPost(likeUser.getId(),blogPost.getId());
+
+        ArgumentCaptor<LikePost> captor = ArgumentCaptor.forClass(LikePost.class);
+        Mockito.verify(postRepository).save(captor.capture());
+
+        LikePost likePost = captor.getValue();
+        Assert.assertEquals(likePost.getPost(),blogPost);
+        Assert.assertEquals(likePost.getUser(),likeUser);
+
     }
 
 }
