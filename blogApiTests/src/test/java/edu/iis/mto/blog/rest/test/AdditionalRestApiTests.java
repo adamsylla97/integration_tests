fa52034685cde8c22fc17f0e5499316035cbf657
Adam Sylla <sylla.adam.97@gmail.com>
2019-06-01 21:59:01 +0200

    *added tests for searching posts

--- blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/AdditionalRestApiTests.java
+++ blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/AdditionalRestApiTests.java
@@ -3,6 +3,7 @@ package edu.iis.mto.blog.rest.test;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import org.apache.http.HttpStatus;
+import org.hamcrest.CoreMatchers;
 import org.json.JSONObject;
 import org.junit.Before;
 import org.junit.Ignore;
@@ -138,16 +139,56 @@ public class AdditionalRestApiTests extends FunctionalTests {
                    .post(POST_LIKE_AUTHOR);
     }
 
-
+    public static final String POST_SEARCH_USER_REMOVED = "/blog/user/4/post";
+    public static final String POST_SEARCH_VALID = "/blog/user/1/post";
 
     @Test
     public void searchingForDeletedUserPostsShouldntBeSuccessful(){
 
+        JSONObject jsonObj = new JSONObject();
+
+        RestAssured.given()
+                   .accept(ContentType.JSON)
+                   .header("Content-Type", "application/json;charset=UTF-8")
+                   .body(jsonObj.toString())
+                   .expect()
+                   .log()
+                   .all()
+                   .statusCode(HttpStatus.SC_BAD_REQUEST)
+                   .when()
+                   .get(POST_SEARCH_USER_REMOVED);
+
     }
 
-    @Ignore
     @Test
     public void searchingForPostsShouldReturnProperAmountOfLikes(){
+
+        JSONObject jsonObj = new JSONObject().put("entry", "test2");
+        RestAssured.given()
+                   .accept(ContentType.JSON)
+                   .header("Content-Type", "application/json;charset=UTF-8")
+                   .body(jsonObj.toString())
+                   .expect()
+                   .log()
+                   .all()
+                   .statusCode(HttpStatus.SC_CREATED)
+                   .when()
+                   .post(POST_API_CONFIRMED);
+
+        JSONObject jsonObj2 = new JSONObject();
+
+        RestAssured.given()
+                   .accept(ContentType.JSON)
+                   .header("Content-Type", "application/json;charset=UTF-8")
+                   .body(jsonObj2.toString())
+                   .expect()
+                   .log()
+                   .all()
+                   .statusCode(HttpStatus.SC_OK)
+                   .and()
+                   .body("likesCount", CoreMatchers.hasItem(0))
+                   .when()
+                   .get(POST_SEARCH_VALID);
 
     }
 
