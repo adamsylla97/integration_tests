74ef3447fc7dc945e9cc5b88ed832e387b30655f
Adam Sylla <sylla.adam.97@gmail.com>
2019-06-01 17:28:06 +0200

    *added dataIntegrityViolationExceptionShouldGenerateHTTPStatus409 test

--- blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/CreateUserTest.java
+++ blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/CreateUserTest.java
@@ -25,4 +25,21 @@ public class CreateUserTest extends FunctionalTests {
                    .when()
                    .post(USER_API);
     }
+
+    @Test
+    public void dataIntegrityViolationExceptionShouldGenerateHTTPStatus409(){
+
+        JSONObject jsonObject = new JSONObject();
+        RestAssured.given()
+                   .accept(ContentType.JSON)
+                   .header("Content-Type","application/json;charset=UTF-8")
+                   .body(jsonObject.toString())
+                   .expect()
+                   .log()
+                   .all()
+                   .statusCode(HttpStatus.SC_CONFLICT)
+                   .when()
+                   .post(USER_API);
+
+    }
 }
