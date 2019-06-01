b3098134175a565e96c9f9bf46fb705acdbc7e4b
Adam Sylla <sylla.adam.97@gmail.com>
2019-06-01 17:45:19 +0200

    *prepared tests signatures

---
+++ blogApiTests/src/test/java/edu/iis/mto/blog/rest/test/AdditionalRestApiTests.java
@@ -0,0 +1,47 @@
+package edu.iis.mto.blog.rest.test;
+
+import org.junit.Test;
+
+public class AdditionalRestApiTests {
+
+    @Test
+    public void emailHasToBeUnique(){
+
+    }
+
+    @Test
+    public void onlyUserWithConfirmedStatusCanAddPost(){
+
+    }
+
+    @Test
+    public void onlyUserWithConfirmedStatusCanLikePost(){
+
+    }
+
+    @Test
+    public void postCannotBeLikedByAuthorOfThatPost(){
+
+    }
+
+    @Test
+    public void relikeOfPostShouldntChangeItsStatus(){
+
+    }
+
+    @Test
+    public void searchingForDeletedUserPostsShouldntBeSuccessful(){
+
+    }
+
+    @Test
+    public void searchingForPostsShouldReturnProperAmountOfLikes(){
+
+    }
+
+    @Test
+    public void lookingForUsersShouldntReturnDeletedUsers(){
+
+    }
+
+}
